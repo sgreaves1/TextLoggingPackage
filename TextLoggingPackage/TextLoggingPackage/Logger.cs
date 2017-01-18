c254945b61c029c5a7ff5aabb29378f16aa2905c
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-18 13:49:08 +0000

    Added logging level enumerator so doesn't log too deep

--- TextLoggingPackage/TextLoggingPackage/Logger.cs
+++ TextLoggingPackage/TextLoggingPackage/Logger.cs
@@ -1,18 +1,24 @@
-﻿namespace TextLoggingPackage
+﻿using System;
+using System.IO;
+
+namespace TextLoggingPackage
 {
     public class Logger
     {
-        public Logger()
-        {
-            // Create file if it doesn't exist
+        public static LoggingLevel ApplicationLoggingLevel = LoggingLevel.Critical;
 
-            // apply the start of the app lines
-        }
-
-        public void Log(string message)
+        public static void Log(string message, string applicationName, LoggingLevel level)
         {
-            // Date Time Application name: Message
-            // 18/01/2017 12:55 SmartHome: Failed to read xml file located at "C:\Users\Sam\SmartHome\Data\Data.xml"! 
+            if ((int)level <= (int)ApplicationLoggingLevel)
+            {
+                // Date Time Application name: Message
+                // 18/01/2017 12:55 SmartHome: Failed to read xml file located at "C:\Users\Sam\SmartHome\Data\Data.xml"! 
+                StreamWriter file = new StreamWriter("Log.txt");
+
+                file.WriteLine(DateTime.Now + applicationName + ": " + message);
+
+                file.Close();
+            }
         }
     }
 }
---
+++ TextLoggingPackage/TextLoggingPackage/LoggingLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace TextLoggingPackage
+{
+    public enum LoggingLevel
+    {
+        Critical = 0,
+        Debug = 1,
+        Trace = 2
+    }
+}

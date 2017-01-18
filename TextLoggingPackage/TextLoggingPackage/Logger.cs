cbdbc62ce470cb0fe9e58bbac69f4f58c72b9e30
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-18 13:56:59 +0000

    Format output string so it contains correct spacing

--- TextLoggingPackage/TextLoggingPackage/Logger.cs
+++ TextLoggingPackage/TextLoggingPackage/Logger.cs
@@ -15,7 +15,7 @@ namespace TextLoggingPackage
                 // 18/01/2017 12:55 SmartHome: Failed to read xml file located at "C:\Users\Sam\SmartHome\Data\Data.xml"! 
                 StreamWriter file = new StreamWriter("Log.txt");
 
-                file.WriteLine(DateTime.Now + applicationName + ": " + message);
+                file.WriteLine(DateTime.Now + " " + applicationName + ": " + message + "\n");
 
                 file.Close();
             }

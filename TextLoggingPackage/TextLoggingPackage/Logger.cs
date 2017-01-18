b3cf841d003090cf73623f0e151de1ae39a85031
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-18 12:57:10 +0000

    Initial commit

---
+++ TextLoggingPackage/TextLoggingPackage/Logger.cs
@@ -0,0 +1,18 @@
+﻿namespace TextLoggingPackage
+{
+    public class Logger
+    {
+        public Logger()
+        {
+            // Create file if it doesn't exist
+
+            // apply the start of the app lines
+        }
+
+        public void Log(string message)
+        {
+            // Date Time Application name: Message
+            // 18/01/2017 12:55 SmartHome: Failed to read xml file located at "C:\Users\Sam\SmartHome\Data\Data.xml"! 
+        }
+    }
+}
